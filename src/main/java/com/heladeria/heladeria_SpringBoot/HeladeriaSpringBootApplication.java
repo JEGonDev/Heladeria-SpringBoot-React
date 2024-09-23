dc421a0131baf414db44a7cc7eaa5c47480a0020
JuanitoZaZa <juanestebangonzalezcaro567@gmail.com>
2024-09-23 10:35:05 -0500

    first commit

---
+++ src/main/java/com/heladeria/heladeria_SpringBoot/HeladeriaSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.heladeria.heladeria_SpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeladeriaSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeladeriaSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/heladeria/heladeria_SpringBoot/HeladeriaSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heladeria.heladeria_SpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeladeriaSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
