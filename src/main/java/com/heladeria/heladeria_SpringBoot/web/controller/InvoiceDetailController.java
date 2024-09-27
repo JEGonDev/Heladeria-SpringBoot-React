79a034ba1f6ba0eac0142c1948c920436708e415
JuanitoZaZa <juanestebangonzalezcaro567@gmail.com>
2024-09-27 15:51:09 -0500

    Heladeria funcionando correctamente

--- src/main/java/com/heladeria/heladeria_SpringBoot/web/controller/InvoiceDetailController.java
+++ src/main/java/com/heladeria/heladeria_SpringBoot/web/controller/InvoiceDetailController.java
@@ -3,8 +3,6 @@ package com.heladeria.heladeria_SpringBoot.web.controller;
 import com.heladeria.heladeria_SpringBoot.domain.InvoiceDetailDomain;
 import com.heladeria.heladeria_SpringBoot.domain.service.InvoiceDetailService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
